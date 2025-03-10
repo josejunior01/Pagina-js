4549ea975cc2b2a09f395d2d55c4240e28b516f2
José Carlos <99501988+josejunior01@users.noreply.github.com>
2025-03-10 19:53:19 -0300

    Pagina-js

---
+++ script.js
@@ -0,0 +1,56 @@
+function changeImage() {
+
+const img = document.getElementById
+('myImage');
+
+const currentSrc = img.getAttribute("src");
+
+if (currentSrc === 'img/javascript.png') {
+
+    img.setAttribute("src","img/java.png")
+
+    img.setAttribute ("alt","aqui tem um img")
+
+}else {
+    img.setAttribute ("src","img/javascript.png");
+    img.setAttribute ("alt","aqui tem uma img")
+}
+
+}
+
+function changImage() {
+
+const img = document.getElementById
+('Imagem')
+ 
+const currentSrc = img.getAttribute('src');
+
+if (currentSrc == 'img/C.png'){
+
+img.setAttribute("src","img/PYTHON.png")
+img.setAttribute("alt","aqui e uma imagem")
+}else {
+    img.setAttribute ("src","img/C.png");
+    img.setAttribute("alt","aqui e uma imagem")
+
+}
+
+}
+function imgImage() {
+
+const img = document.getElementById
+('img')
+ 
+const currentSrc = img.getAttribute('src');
+
+if (currentSrc == 'img/sql.png'){
+
+img.setAttribute("src","img/php.jpg")
+img.setAttribute("alt","aqui e uma imagem")
+}else {
+    img.setAttribute ("src","img/sql.png");
+    img.setAttribute("alt","aqui e uma imagem")
+
+}
+
+}
